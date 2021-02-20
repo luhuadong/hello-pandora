1a1352dd38285fae85724f597d8625d6c356cb56
luhuadong <luhuadong@163.com>
2021-02-20 09:47:47 +0800

    add 0wastehome logo

--- applications/lcd_sample.c
+++ applications/lcd_sample.c
@@ -15,6 +15,7 @@
 #ifdef BSP_USING_SPI_LCD
 #include <drv_lcd.h>
 #include <rttlogo.h>
+#include <logo.h>
 
 static int lcd_sample(void)
 {
@@ -22,25 +23,29 @@ static int lcd_sample(void)
     lcd_clear(WHITE);
 
     /* show RT-Thread logo */
-    lcd_show_image(0, 0, 240, 69, image_rttlogo);
+    //lcd_show_image(0, 0, 240, 69, image_rttlogo);
+    lcd_show_image(0, 0, 240, 240, gImage_0wastehome);
     
     /* set background and foreground color */
     lcd_set_color(WHITE, BLACK);
 
     /* show string */
-    lcd_show_string(10, 69, 16, "Hello, Bocon!");
-    lcd_show_string(10, 69+16, 24, "Bocon");
-    lcd_show_string(10, 69+16+24, 32, "Bocon");
+    //lcd_show_string(10, 69, 16, "Hello, Bocon!");
+    //lcd_show_string(10, 69+16, 24, "Bocon");
+    //lcd_show_string(10, 69+16+24, 32, "Bocon");
     
     /* draw a line */
-    lcd_draw_line(0, 69+16+24+32, 240, 69+16+24+32);
+    //lcd_draw_line(0, 69+16+24+32, 240, 69+16+24+32);
     
     /* draw some circles */
+    /*
+
     lcd_draw_point(120, 194);
     for (int i = 0; i < 46; i += 4)
     {
         lcd_draw_circle(120, 194, i);
     }
+    */
     
     return RT_EOK;
 }
